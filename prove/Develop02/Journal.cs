e6d6f18bc0152456b0395dcf118109b41bf0fc27
henridivy <henridivine@gmail.com>
2023-05-03 09:36:41 +0700

    Added password protection to journals and a test journal

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -39,38 +39,62 @@ public class Journal
     // create a method that saves all entries so far to a file
     public void SaveFile(Journal journalName)
     {
-        Console.WriteLine("What is the name of the file?");
+        Console.Write("What is the name of the file? ");
         string filename = Console.ReadLine();
+        // assign a password to this journal
+        Console.Write("What's the password? ");
+        string password = Console.ReadLine();
 
         using (StreamWriter outputFile = new StreamWriter(filename))
         {
             foreach (Entry entry in journalName._entries)
             {
-                outputFile.WriteLine($"{entry._date},{entry._prompt},{entry._response}");
+                outputFile.WriteLine($"{entry._date}||{entry._prompt}||{entry._response}");
             }
 
+            // write the password after writing all the entries
+            outputFile.WriteLine();
+            outputFile.WriteLine($"Password: {password}");
         }
     }
 
-    // create a method that loads an existing file with journal entries, where you can continue to add
+    // create a method that loads an existing file with journal entries, where you can continue to add entries
     public void LoadFile(Journal journalName)
     {
-        Console.WriteLine("What is the name of the file?");
+        Console.Write("What is the name of the file? ");
         string filename = Console.ReadLine();
+
+        // ask the user for a password to access this journal
+        Console.Write("What's the password? ");
+        string password = Console.ReadLine();
+
         string[] lines = System.IO.File.ReadAllLines(filename);
 
-        // clear the previous saved entries so files don't combine
-        _entries.Clear();
+        int lastIndex = lines.Count() - 1;
 
-        foreach (string line in lines)
+        if (lines[lastIndex] == $"Password: {password}")
         {
-            string[] parts = line.Split(",");
+            // clear the previous saved entries so files don't combine
+            _entries.Clear();
 
-            string date = parts[0];
-            string prompt = parts[1];
-            string response = parts[2];
+            foreach (string line in lines)
+            {
+                string[] parts = line.Split("||");
 
-            journalName.CreateJournalEntry(prompt, date, response);
+                // checks if the line has more than one part, this ensures it doesn't include the password
+                if (parts.Count() > 1)
+                {
+                    string date = parts[0];
+                    string prompt = parts[1];
+                    string response = parts[2];
+                    
+                    journalName.CreateJournalEntry(prompt, date, response);
+                }
+            }
+        }
+        else 
+        {
+            Console.WriteLine("Password is incorrect.");
         }
     }
 }
