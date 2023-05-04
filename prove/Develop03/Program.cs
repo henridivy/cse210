752dc17a22978a4b05104e7c5a6ebac795c8212f
henridivy <henridivine@gmail.com>
2023-05-04 09:57:18 +0700

    Started W03 Prove - Scripture Memorizer Program

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        Console.WriteLine("Welcome to the Scripture Memorizer Program!");
+        Console.WriteLine("Please choose the scripture you would like to master.");
+        Console.WriteLine();
+
+        // List<string> _menu = new List<string>
+        // {
+        //     "1. Moses 1:39",
+        //     "2. "
+        // };
+
+        IDictionary<string, string> scriptures = new Dictionary<string, string>();
+        
     }
 }
---
+++ prove/Develop03/Reference.cs
@@ -0,0 +1,28 @@
+public class Reference
+{
+    private string _book;
+    private string _chapter;
+    private string _startVerse;
+    private string _endVerse;
+
+    public Reference(string book, string chapter, string start)
+    {
+        _book = book;
+        _chapter = chapter;
+        _startVerse = start;
+        _endVerse = "";
+    }
+
+    public Reference(string book, string chapter, string start, string end)
+    {
+        _book = book;
+        _chapter = chapter;
+        _startVerse = start;
+        _endVerse = "-" + end;
+
+    // public string GetReference()
+    // {
+    //     string reference = $"{_book} {_chapter}:{_startVerse}{_endVerse}";
+    //     return reference;
+    // }
+}
